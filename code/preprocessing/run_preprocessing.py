1eecb7730b7e12d94015754ffbd6f017eb66ea7b
lbechberger <lbechberger@uos.de>
2021-09-28 17:05:19 +0200

    stub for run_preprocessing.py
    need to implement actual preprocessing steps

---
+++ code/preprocessing/run_preprocessing.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Runs the specified collection of preprocessing steps
+
+Created on Tue Sep 28 16:43:18 2021
+
+@author: lbechberger
+"""
+
+import argparse
+import pandas as pd
+
+# setting up CLI
+parser = argparse.ArgumentParser(description = "Various preprocessing steps")
+parser.add_argument("input_file", help = "path to the input csv file")
+parser.add_argument("output_file", help = "path to the output csv file")
+args = parser.parse_args()
+
+# load data
+df = pd.read_csv(args.input_file)
+
+
+# TODO: call all preprocessing steps
+
+
+# store the results
+df.to_csv(args.output_file)
