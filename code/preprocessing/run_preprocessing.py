248112e28c4fcea451e2f4653f31940ae654bec4
lbechberger <lbechberger@uos.de>
2021-09-29 10:37:10 +0200

    exporting preprocessing pipeline to pickle

--- code/preprocessing/run_preprocessing.py
+++ code/preprocessing/run_preprocessing.py
@@ -8,8 +8,9 @@ Created on Tue Sep 28 16:43:18 2021
 @author: lbechberger
 """
 
-import argparse, csv
+import argparse, csv, pickle
 import pandas as pd
+from sklearn.pipeline import make_pipeline
 from code.preprocessing.punctuation_remover import PunctuationRemover
 
 # setting up CLI
@@ -17,6 +18,7 @@ parser = argparse.ArgumentParser(description = "Various preprocessing steps")
 parser.add_argument("input_file", help = "path to the input csv file")
 parser.add_argument("output_file", help = "path to the output csv file")
 parser.add_argument("-p", "--punctuation", action = "store_true", help = "remove punctuation")
+parser.add_argument("-e", "--export", help = "create a pipeline and export to the given location", default = None)
 args = parser.parse_args()
 
 # load data
@@ -33,3 +35,9 @@ for preprocessor in preprocessors:
 
 # store the results
 df.to_csv(args.output_file, index = False, quoting = csv.QUOTE_NONNUMERIC, line_terminator = "\n")
+
+# create a pipeline if necessary and store it as pickle file
+if args.export is not None:
+    pipeline = make_pipeline(*preprocessors)
+    with open(args.export, 'wb') as f_out:
+        pickle.dump(pipeline, f_out)
