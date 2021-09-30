e4a47a0141690c5a99f4918042d84292eb9e4939
lbechberger <lbechberger@uos.de>
2021-09-30 09:33:34 +0200

    less verbose wget output for downloading raw data

--- code/load_data.sh
+++ code/load_data.sh
@@ -4,7 +4,7 @@
 mkdir -p data/raw/
 
 # download the three csv files
-wget https://myshare.uni-osnabrueck.de/f/3e5276caf72b46e7ace2/?dl=1 -O data/raw/data_analysis.csv
-wget https://myshare.uni-osnabrueck.de/f/e620aff7719948d18a52/?dl=1 -O data/raw/data_science.csv
-wget https://myshare.uni-osnabrueck.de/f/9ddaab064c68483e9bff/?dl=1 -O data/raw/data_visualization.csv
+wget -nv https://myshare.uni-osnabrueck.de/f/3e5276caf72b46e7ace2/?dl=1 -O data/raw/data_analysis.csv
+wget -nv https://myshare.uni-osnabrueck.de/f/e620aff7719948d18a52/?dl=1 -O data/raw/data_science.csv
+wget -nv https://myshare.uni-osnabrueck.de/f/9ddaab064c68483e9bff/?dl=1 -O data/raw/data_visualization.csv
 
