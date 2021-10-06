660536f8780a41929600c612b9ef4a078f70e70e
Manar Ali <maro.ali.990@gmail.com>
2021-10-07 00:53:38 +0200

    Update the branch with origin.

--- code/preprocessing.sh
+++ code/preprocessing.sh
@@ -3,13 +3,16 @@
 # create directory if not yet existing
 mkdir -p data/preprocessing/split/
 
+# need to install all NLTK models
+python -m nltk.dowloader all
+
 # add labels
 echo "  creating labels"
 python -m code.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
 
 # other preprocessing (removing punctuation etc.)
 echo "  general preprocessing"
-python -m code.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation -e data/preprocessing/pipeline.pickle
+python -m code.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation --tokenize -e data/preprocessing/pipeline.pickle
 
 # split the data set
 echo "  splitting the data set"
