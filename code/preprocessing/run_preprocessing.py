5d3508274a0e839f51f0b55bb951863895c891a8
Dennis Hesenkamp <83503798+dhesenkamp@users.noreply.github.com>
2021-10-06 18:50:58 +0200

    Added tweet tokenization
    Implemented a tokenizer in tokenizer.py by hand as seen in the lecture. Modified all corresponding files involved in the preprocessing.

--- code/preprocessing/run_preprocessing.py
+++ code/preprocessing/run_preprocessing.py
@@ -6,18 +6,23 @@ Runs the specified collection of preprocessing steps
 Created on Tue Sep 28 16:43:18 2021
 
 @author: lbechberger
+modified: dhesenkamp
 """
 
 import argparse, csv, pickle
 import pandas as pd
 from sklearn.pipeline import make_pipeline
 from code.preprocessing.punctuation_remover import PunctuationRemover
+from code.preprocessing.tokenizer import Tokenizer
+from code.util import SUFFIX_TOKENIZED, COLUMN_TWEET
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Various preprocessing steps")
 parser.add_argument("input_file", help = "path to the input csv file")
 parser.add_argument("output_file", help = "path to the output csv file")
 parser.add_argument("-p", "--punctuation", action = "store_true", help = "remove punctuation")
+parser.add_argument("-t", "--tokenize", action = "store_true", help = "tokenize given column into individual words")
+parser.add_argument("--tokenize_input", help = "input column to tokenize", default = COLUMN_TWEET)
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
 args = parser.parse_args()
 
@@ -28,6 +33,8 @@ df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator
 preprocessors = []
 if args.punctuation:
     preprocessors.append(PunctuationRemover())
+if args.tokenize:
+    preprocessors.append(Tokenizer(args.tokenize_input, args.tokenize_input + SUFFIX_TOKENIZED))
 
 # call all preprocessing steps
 for preprocessor in preprocessors:
@@ -41,3 +48,4 @@ if args.export_file is not None:
     pipeline = make_pipeline(*preprocessors)
     with open(args.export_file, 'wb') as f_out:
         pickle.dump(pipeline, f_out)
+        
