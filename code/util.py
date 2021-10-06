493743c394a95551f0954609731fea92bab46c28
Lara McDonald <mccilara@gmail.com>
2021-10-06 21:35:40 +0200

    Added expansion of contractions

--- code/util.py
+++ code/util.py
@@ -16,5 +16,6 @@ COLUMN_RETWEETS = "retweets_count"
 # column names of novel columns for preprocessing
 COLUMN_LABEL = "label"
 COLUMN_PUNCTUATION = "tweet_no_punctuation"
+COLUMN_CONTRACTIONS = "tweet_no_contractions"
 
 SUFFIX_TOKENIZED = "_tokenized"
