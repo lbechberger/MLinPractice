08e75c8b55e2e8f52aa06575321800262eb730ce
Manar Ali <maro.ali.990@gmail.com>
2021-10-07 00:57:08 +0200

    Update util.py

--- code/util.py
+++ code/util.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Utility file for collecting frequently used constants and helper functions.
-
 Created on Wed Sep 29 10:50:36 2021
-
 @author: lbechberger
 """
 
@@ -16,3 +14,5 @@ COLUMN_RETWEETS = "retweets_count"
 # column names of novel columns for preprocessing
 COLUMN_LABEL = "label"
 COLUMN_PUNCTUATION = "tweet_no_punctuation"
+
+SUFFIX_TOKENIZED = "_tokenized"
