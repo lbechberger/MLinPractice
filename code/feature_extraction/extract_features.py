c61cef6a431277338073befae5fe33f6a09bd097
Marcel Klehr <mklehr@gmx.net>
2021-10-07 20:21:54 +0200

    Implement sentiment analysis

--- code/feature_extraction/extract_features.py
+++ code/feature_extraction/extract_features.py
@@ -13,6 +13,7 @@ import pandas as pd
 import numpy as np
 from code.feature_extraction.character_length import CharacterLength
 from code.feature_extraction.feature_collector import FeatureCollector
+from code.feature_extraction.sentiment import Sentiment
 from code.util import COLUMN_TWEET, COLUMN_LABEL
 
 
@@ -23,6 +24,7 @@ parser.add_argument("output_file", help = "path to the output pickle file")
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
 parser.add_argument("-i", "--import_file", help = "import an existing pipeline from the given location", default = None)
 parser.add_argument("-c", "--char_length", action = "store_true", help = "compute the number of characters in the tweet")
+parser.add_argument("-s", "--sentiment", action = "store_true", help = "compute the tweet sentiment")
 args = parser.parse_args()
 
 # load data
@@ -40,7 +42,11 @@ else:    # need to create FeatureCollector manually
     if args.char_length:
         # character length of original tweet (without any changes)
         features.append(CharacterLength(COLUMN_TWEET))
-    
+
+    if args.sentiment:
+        # sentiment of original tweet (without any changes)
+        features.append(Sentiment(COLUMN_TWEET))
+
     # create overall FeatureCollector
     feature_collector = FeatureCollector(features)
     
