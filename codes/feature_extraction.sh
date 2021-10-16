1a9a399b7e286a3fa232b0a28177b3721fd229b1
Samuel <samuel.hagemann@gmx.de>
2021-10-16 17:59:58 +0200

    Added Photo Feature

---
+++ codes/feature_extraction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create directory if not yet existing
+mkdir -p data/feature_extraction/
+
+# run feature extraction on training set (may need to fit extractors)
+echo "  training set"
+python -m codes.feature_extraction.extract_features data/preprocessing/split/training.csv data/feature_extraction/training.pickle -e data/feature_extraction/pipeline.pickle --char_length --photo_bool
+
+# run feature extraction on validation set and test set (with pre-fit extractors)
+echo "  validation set"
+python -m codes.feature_extraction.extract_features data/preprocessing/split/validation.csv data/feature_extraction/validation.pickle -i data/feature_extraction/pipeline.pickle
+echo "  test set"
+python -m codes.feature_extraction.extract_features data/preprocessing/split/test.csv data/feature_extraction/test.pickle -i data/feature_extraction/pipeline.pickle
