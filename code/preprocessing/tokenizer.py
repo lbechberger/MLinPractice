a3333a89b8dc6d7ee21f4c605167b56c5f140fc2
Reem Farah <reem.farah@visable.com>
2021-10-16 15:18:31 +0200

    modified the tokenizer

--- code/preprocessing/tokenizer.py
+++ code/preprocessing/tokenizer.py
@@ -9,45 +9,51 @@ Created on Wed Oct  6 13:59:54 2021
 """
 
 from code.preprocessing.preprocessor import Preprocessor
+import string
 import nltk
+
 
 class Tokenizer(Preprocessor):
     """Tokenizes the given input column into individual words."""
-    
-    def __init__(self, input_column, output_column): 
+
+    def __init__(self, input_column, output_column):
         """Initialize the Tokenizer with the given input and output column."""
         super().__init__([input_column], output_column)
-    
-    # don't need to implement _set_variables(), since no variables to set
-    
+
+    def _set_variables(self, inputs):
+        self.urls = ["http", "https", "www"]
+        self.special_characters = ['@', '#']
+        self.punctuation = [x for x in string.punctuation[1:-1]]
     def _get_values(self, inputs):
         """Tokenize the tweet."""
 
-        # all_input_tokenized = []
-        # print(len(inputs), inputs[0])
-        # if len(inputs) > 1:
-        #     inputs = inputs[0]
-        #     print(inputs)
-        #     for tweet in inputs:
-        #         tokenized = []
-        #         sentences = nltk.sent_tokenize(tweet)
-        #         for sentence in sentences:
-        #             words = nltk.word_tokenize(sentence)
-        #             tokenized.append(words)
-        #     tokenized = [token for sublist in tokenized for token in sublist]
-        #     all_input_tokenized.append(tokenized)
-        # else:
-        #     words = nltk.word_tokenize(inputs[0][0])
-
-        #     all_input_tokenized.append(words)
         tokenized = []
         for tweet in inputs[0]:
             sentences = nltk.sent_tokenize(tweet.lower())
             tokenized_tweet = []
             for sentence in sentences:
                 words = nltk.word_tokenize(sentence)
-                tokenized_tweet += words
-            
+                words = self._delete_mentions(words)
+                remove_urls_hashtags_mentions = [
+                    word for word in words if word[0] not in self.punctuation and word[:4] not in self.urls and word[:3] not in self.urls]
+                
+                tokenized_tweet += remove_urls_hashtags_mentions
+
             tokenized.append(tokenized_tweet)
 
         return tokenized
+
+    def _delete_mentions(self, words):
+        """Deletes the hashtags and the mentions"""
+        
+        no_mentions = []
+
+        for i in range(len(words)):
+            if words[i][0] not in self.special_characters:
+                if i > 0:
+                    if words[i-1][0] not in self.special_characters:
+                        no_mentions.append(words[i])
+                elif i == 0:
+                    no_mentions.append(words[i])
+
+        return no_mentions
--- test/preprocessing/tokenizer_test.py
+++ test/preprocessing/tokenizer_test.py
@@ -10,15 +10,17 @@ import unittest
 import pandas as pd
 from code.preprocessing.tokenizer import Tokenizer
 
+
 class TokenizerTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.INPUT_COLUMN = "input"
         self.OUTPUT_COLUMN = "output"
         self.tokenizer = Tokenizer(self.INPUT_COLUMN, self.OUTPUT_COLUMN)
-    
+
     def test_input_columns(self):
-        self.assertListEqual(self.tokenizer._input_columns, [self.INPUT_COLUMN])
+        self.assertEqual(self.tokenizer._input_columns, [
+                             self.INPUT_COLUMN])
 
     def test_output_column(self):
         self.assertEqual(self.tokenizer._output_column, self.OUTPUT_COLUMN)
@@ -26,26 +28,37 @@ class TokenizerTest(unittest.TestCase):
     def test_tokenization_single_sentence(self):
         input_text = "This is an example sentence"
         output_text = ['this', 'is', 'an', 'example', 'sentence']
-        
+
         input_df = pd.DataFrame()
         input_df[self.INPUT_COLUMN] = [input_text]
-        
+
         tokenized = self.tokenizer.fit_transform(input_df)
         self.assertEqual(tokenized[self.OUTPUT_COLUMN][0], output_text)
 
     def test_tokenization_several_sentences(self):
-        input_text = ["This is an example sentence", "I want for my tweet to go viral", "Can we talk about global warming?"]
+        input_text = ["This is an example sentence",
+                      "I want for my tweet to go viral", "Can we talk about global warming?"]
         output_text = [['this', 'is', 'an', 'example', 'sentence'], ["i", "want", "for", "my", "tweet", "to", "go", "viral"],
-                        ["can", "we", 'talk', 'about', 'global', 'warming', '?']]
-        
+                       ["can", "we", 'talk', 'about', 'global', 'warming']]
+
         input_df = pd.DataFrame(input_text, columns=[self.INPUT_COLUMN])
 
         tokenized = self.tokenizer.fit_transform(input_df)
-        print(tokenized[self.OUTPUT_COLUMN][0])
         for i in range(3):
-            self.assertEquals(tokenized[self.OUTPUT_COLUMN][i], output_text[i])
-    
+            self.assertEqual(tokenized[self.OUTPUT_COLUMN][i], output_text[i])
+
+    def test_tokenization_several_sentences_with_urls_hashtags_mentions(self):
+        input_text = ["This is an example sentence https://www.atlassian.com/git/tutorials/using-branches",
+                      "I want for my tweet to go viral @twitter", "Can we talk about global warming? #hashtag"]
+        output_text = [['this', 'is', 'an', 'example', 'sentence'], ["i", "want", "for", "my", "tweet", "to", "go", "viral"],
+                       ["can", "we", 'talk', 'about', 'global', 'warming']]
+
+        input_df = pd.DataFrame(input_text, columns=[self.INPUT_COLUMN])
+
+        tokenized = self.tokenizer.fit_transform(input_df)
+        for i in range(3):
+            self.assertEqual(tokenized[self.OUTPUT_COLUMN][i], output_text[i])
+
 
 if __name__ == '__main__':
     unittest.main()
-    
