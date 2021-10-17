2b8377d28879f29bb308be32037a8d904dbe7ff5
Samuel <samuel.hagemann@gmx.de>
2021-10-17 19:06:11 +0200

    Added replies count feature; added 'help' description

--- codes/util.py
+++ codes/util.py
@@ -13,6 +13,7 @@ COLUMN_TWEET = "tweet"
 COLUMN_LIKES = "likes_count"
 COLUMN_RETWEETS = "retweets_count"
 COLUMN_PHOTOS = "photos"
+COLUMN_REPLIES = "replies_count"
 
 # column names of novel columns for preprocessing
 COLUMN_LABEL = "label"
