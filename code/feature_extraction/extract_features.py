6f372f7e18ab0c2da0a826d92beab6191880830d
avocardio <mkalcher@uni-osnabrueck.de>
2021-10-19 19:22:35 -0500

    word2vec feature WIP

--- code/feature_extraction/extract_features.py
+++ code/feature_extraction/extract_features.py
@@ -20,6 +20,7 @@ from code.feature_extraction.feature_collector import FeatureCollector
 from code.feature_extraction.photo_bool import PhotoBool
 from code.feature_extraction.video_bool import VideoBool
 from code.feature_extraction.replies_count import RepliesCount
+from code.feature_extraction.word2vec import Word2Vec
 from code.util import COLUMN_TWEET, COLUMN_LABEL, COLUMN_PREPROCESS, COLUMN_PHOTOS, COLUMN_REPLIES, COLUMN_VIDEO
 
 
@@ -41,11 +42,14 @@ parser.add_argument("--video_bool", action="store_true",
 		    help="tells whether the tweet contains a video or not")
 parser.add_argument("--replies_count", action="store_true", 
 		    help="compute the amount of replies of the tweet")
+parser.add_argument("--word2vec", action="store_true", 
+		    help="compute the semantic distance of words to given keywords")
 args = parser.parse_args()
 
 # load data
 df = pd.read_csv(args.input_file, quoting=csv.QUOTE_NONNUMERIC,
                  lineterminator="\n")
+df = df[0:1000]
 
 if args.import_file is not None:
     # simply import an exisiting FeatureCollector
@@ -74,6 +78,8 @@ else:    # need to create FeatureCollector manually
     if args.replies_count:
         # how many replies does the tweet have
         features.append(RepliesCount(COLUMN_REPLIES))
+    if args.word2vec:
+        features.append(Word2Vec('preprocess_col_tokenized'))
 
     # create overall FeatureCollector
     feature_collector = FeatureCollector(features)
@@ -93,6 +99,8 @@ label_array = label_array.reshape(-1, 1)
 # store the results
 results = {"features": feature_array, "labels": label_array,
            "feature_names": feature_collector.get_feature_names()}
+
+print(results['features'].ravel())
 
 with open(args.output_file, 'wb') as f_out:
     pickle.dump(results, f_out)
---
+++ code/feature_extraction/word2vec.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Simple feature that counts the number of characters in the given column.
+
+Created on Wed Sep 29 12:29:25 2021
+
+@author: lbechberger
+"""
+
+import numpy as np
+import gensim.downloader as api
+import pandas as pd
+import ast
+from code.feature_extraction.feature_extractor import FeatureExtractor
+
+# class for extracting the character-based length as a feature
+class Word2Vec(FeatureExtractor):
+    
+    # constructor
+    def __init__(self, input_column):
+        super().__init__([input_column], "{0}_word2vec".format(input_column))
+    
+    # don't need to fit, so don't overwrite _set_variables()
+    
+    # compute the word length based on the inputs
+    def _get_values(self, inputs):
+        
+        embeddings = api.load('word2vec-google-news-300') # Try glove-twitter-200 for classifier 
+        keywords = ['coding','free','algorithms','statistics'] # DataScience, BigData, deeplearning, machinelearning not present
+
+        tokens = inputs[0].apply(lambda x: list(ast.literal_eval(x))) # Column from Series to list
+
+        similarities = []
+        
+        for rows in tokens:
+            sim = []
+            for word in keywords:
+                for item in rows:
+                    try:
+                        sim.append(embeddings.similarity(item,word))
+                    except KeyError:
+                        pass
+            # similarities.append(max(sim)-np.std(sim))
+            similarities.append(np.mean(sim)) # try median
+    
+        result = similarities.reshape(-1,1)
+        return result
