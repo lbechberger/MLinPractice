bf5cd3ad731ad83cc7affe1cd1ed33cafa0d4c06
Reem Farah <reem.farah@visable.com>
2021-10-22 15:26:50 +0200

    updated Documentation.md and formatted some files

--- code/preprocessing/lemmatizer.py
+++ code/preprocessing/lemmatizer.py
@@ -13,7 +13,8 @@ from code.preprocessing.preprocessor import Preprocessor
 import nltk
 import ast
 from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer 
+from nltk.stem import WordNetLemmatizer
+
 
 class Lemmatizer(Preprocessor):
     """Provides the lemmas of the words of the tweet using their POS."""
@@ -25,17 +26,17 @@ class Lemmatizer(Preprocessor):
     def _get_wordnet_pos(self, tweet):
         pos = dict(nltk.pos_tag(tweet))
         # change the POSes so the lemmatizer can make use of them
-        changed_pos = dict([(name, self.tag_dict.get(pos)) for name, pos in pos.items()])
-
+        changed_pos = dict([(name, self.tag_dict.get(pos))
+                           for name, pos in pos.items()])
 
         return changed_pos
 
     def _set_variables(self, inputs):
         self.lemmatizer = WordNetLemmatizer()
-        self.tag_dict = {"MD" : "a", "JJ" : "a", "JJR" : "a", "JJS" : "a",
-                    "NN" : "n", "NNS" : "n", "NNP" : "n", "NNPS" : "n", "FW": "n", "MD" : "n",
-                    "VB" : "v", "VBD" : "v", "VBG" : "v", "VBN" : "v", "VBP" : "v", "VBZ" : "v",
-                    "RB" : "r", "RBR" : "r", "RBS" : "r", "RP" : "r"}
+        self.tag_dict = {"MD": "a", "JJ": "a", "JJR": "a", "JJS": "a",
+                         "NN": "n", "NNS": "n", "NNP": "n", "NNPS": "n", "FW": "n", "MD": "n",
+                         "VB": "v", "VBD": "v", "VBG": "v", "VBN": "v", "VBP": "v", "VBZ": "v",
+                         "RB": "r", "RBR": "r", "RBS": "r", "RP": "r"}
 
     def _get_values(self, inputs):
         """Finds the Lemmas of the tweet's words."""
@@ -45,7 +46,8 @@ class Lemmatizer(Preprocessor):
             if isinstance(tweet, str):
                 tweet = ast.literal_eval(tweet)
             pos_tweet = self._get_wordnet_pos(tweet)
-            row = [self.lemmatizer.lemmatize(word, pos) if pos else self.lemmatizer.lemmatize(word) for word,pos in pos_tweet.items()]
+            row = [self.lemmatizer.lemmatize(word, pos) if pos else self.lemmatizer.lemmatize(
+                word) for word, pos in pos_tweet.items()]
             lemmatized.append(row)
 
         return lemmatized
--- code/preprocessing/stopwords_remover.py
+++ code/preprocessing/stopwords_remover.py
@@ -11,9 +11,10 @@ import contractions
 from nltk.corpus import stopwords
 from code.preprocessing.preprocessor import Preprocessor
 
+
 class StopwordsRemover(Preprocessor):
     # constructor
-    def __init__(self, input_column, output_column): 
+    def __init__(self, input_column, output_column):
         # input column "tweet", new output column
         super().__init__([input_column], output_column)
 
@@ -24,29 +25,36 @@ class StopwordsRemover(Preprocessor):
         self._stopwords_de = stopwords.words('german')
         self._stopwords_fr = stopwords.words('french')
         self._stopwords_es = stopwords.words('spanish')
-    
+
     # get preprocessed column based on data frame and internal variables
     def _get_values(self, inputs):
         inputs = inputs[0]
         # replace stopwords and punctuations with empty string
         tweets_withno_stopwords = []
-        language = inputs.iloc[:,1][0]
-        for tweet in inputs.iloc[:,0]:
+        language = inputs.iloc[:, 1][0]
+        for tweet in inputs.iloc[:, 0]:
             if isinstance(tweet, list) and len(tweet) == 1:
                 tweet = tweet[0]
             tweet_withno_stopwords = []
-                
+
             if 'en' in language:
-                tweet_withno_contractions = [contractions.fix(word) for word in tweet]
-                tweet_withno_stopwords = [word.split(" ") for word in tweet_withno_contractions if word.lower() not in self._stopwords_en]
-                tweet_withno_stopwords_splitted = [splitted if isinstance(word, list) else word for word in tweet_withno_stopwords for splitted in word]
-                tweet_withno_stopwords = [word for word in tweet_withno_stopwords_splitted if word.lower() not in self._stopwords_en]
+                tweet_withno_contractions = [
+                    contractions.fix(word) for word in tweet]
+                tweet_withno_stopwords = [word.split(
+                    " ") for word in tweet_withno_contractions if word.lower() not in self._stopwords_en]
+                tweet_withno_stopwords_splitted = [splitted if isinstance(
+                    word, list) else word for word in tweet_withno_stopwords for splitted in word]
+                tweet_withno_stopwords = [
+                    word for word in tweet_withno_stopwords_splitted if word.lower() not in self._stopwords_en]
             elif 'de' in language:
-                tweet_withno_stopwords = [word for word in tweet if word.lower() not in self._stopwords_de]
+                tweet_withno_stopwords = [
+                    word for word in tweet if word.lower() not in self._stopwords_de]
             elif 'fr' in language:
-                tweet_withno_stopwords = [word for word in tweet if word.lower() not in self._stopwords_fr]
+                tweet_withno_stopwords = [
+                    word for word in tweet if word.lower() not in self._stopwords_fr]
             elif 'es' in language:
-                tweet_withno_stopwords = [word for word in tweet if word.lower() not in self._stopwords_es]
+                tweet_withno_stopwords = [
+                    word for word in tweet if word.lower() not in self._stopwords_es]
 
             tweets_withno_stopwords.append(tweet_withno_stopwords)
         return tweets_withno_stopwords
