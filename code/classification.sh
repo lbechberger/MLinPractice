ed23f5c50f4aab9afed44cb620a437661aed50c4
Yannik101010 <80115275+Yannik101010@users.noreply.github.com>
2021-10-23 21:39:43 +0200

    Update run_classifier.py, classification.sh
    Added a balanced accuracy score

--- code/classification.sh
+++ code/classification.sh
@@ -5,12 +5,12 @@ mkdir -p data/classification/
 
 # run feature extraction on training set (may need to fit extractors)
 echo "  training set"
-#python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --svm 'rbf' -s 42 -a -k -f1
-python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --randforest 50 -s 42 -a -k -f1
+#python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --svm 'rbf' -s 42 -a -k -f1 -ba
+python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --randforest 50 -s 42 -a -k -f1 -ba
 
 # run feature extraction on validation set (with pre-fit extractors)
 echo "  validation set"
-#python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle -a -k -f1
-python -m code.classification.run_classifier data/feature_extraction/validation.pickle -i data/classification/classifier.pickle -a -k -f1
+#python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle -a -k -f1 -ba
+python -m code.classification.run_classifier data/feature_extraction/validation.pickle -i data/classification/classifier.pickle -a -k -f1 -ba
 
 # don't touch the test set, yet, because that would ruin the final generalization experiment!
