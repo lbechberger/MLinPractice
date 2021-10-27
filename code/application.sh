1a0bb8c623671ab87a2354f19325d1fd6b741c6e
Samuel <samuel.hagemann@gmx.de>
2021-10-27 19:04:54 +0200

    Added and improved application, clean code

--- code/application.sh
+++ code/application.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # execute the application with all necessary pickle files
+# dim_red not used: data/dimensionality_reduction/pipeline.pickle
 echo "Starting the application..."
-python -m code.application.application data/preprocessing/pipeline.pickle data/feature_extraction/pipeline.pickle data/dimensionality_reduction/pipeline.pickle data/classification/classifier.pickle
+python -m code.application.application data/preprocessing/pipeline.pickle data/feature_extraction/pipeline.pickle data/classification/classifier.pickle
