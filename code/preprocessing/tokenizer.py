a8e2a19774c60a9d3a8cce3084efd41cc01ebca0
ReemS3 <86101529+ReemS3@users.noreply.github.com>
2021-10-29 22:07:24 +0200

    Update tokenizer.py

--- code/preprocessing/tokenizer.py
+++ code/preprocessing/tokenizer.py
@@ -2,23 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Tokenize the tweet into individual words.
-<<<<<<< HEAD
-<<<<<<< HEAD
-Created on Wed Oct  6 07:49:27 2021
-=======
 
-Created on Wed Oct  6 13:59:54 2021
-
-<<<<<<< HEAD
->>>>>>> c9d63eb (created Tokenizer class)
-@author: lbechberger
-=======
-@author: lbechberger/rfarah
->>>>>>> 0898e45 (add the lemmatizer and its test)
-=======
 Created on Wed Oct  6 07:49:27 2021
+
 @author: lbechberger
->>>>>>> de1e52e (Update the branch with origin.)
 """
 
 from code.preprocessing.preprocessor import Preprocessor
@@ -27,87 +14,25 @@ import nltk
 
 class Tokenizer(Preprocessor):
     """Tokenizes the given input column into individual words."""
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     def __init__(self, input_column, output_column):
         """Initialize the Tokenizer with the given input and output colum."""
-=======
-    
-    def __init__(self, input_column, output_column): 
-=======
-
-    def __init__(self, input_column, output_column):
->>>>>>> a3333a8 (modified the tokenizer)
-=======
-
-    def __init__(self, input_column, output_column):
-=======
-    
-    def __init__(self, input_column, output_column): 
->>>>>>> 49c39fa (resolve the conflict)
->>>>>>> 7105dbc (resolve the conflict)
-=======
-
-    def __init__(self, input_column, output_column):
->>>>>>> 828d2d3 (fix merge issues)
-        """Initialize the Tokenizer with the given input and output column."""
->>>>>>> 2854caf (modified files and testing added)
         super().__init__([input_column], output_column)
 
-<<<<<<< HEAD
-    # don't need to implement _set_variables()
-
-=======
     def _set_variables(self, inputs):
         self.urls = ["http", "https", "www"]
         self.special_characters = ['@', '#']
         self.punctuation = [x for x in string.punctuation[1:-1]]
-<<<<<<< HEAD
->>>>>>> a3333a8 (modified the tokenizer)
-=======
 
->>>>>>> 0898e45 (add the lemmatizer and its test)
     def _get_values(self, inputs):
-<<<<<<< HEAD
-        """Tokenize the tweet."""
-<<<<<<< HEAD
-        tokenized = []
-
-=======
-=======
         """Tokenizes the tweet and get rid of mentions, hashtags and numbers"""
->>>>>>> acba8f0 (modified the comments of the files)
-
+        
         tokenized = []
->>>>>>> 2854caf (modified files and testing added)
         for tweet in inputs[0]:
             sentences = nltk.sent_tokenize(tweet.lower())
             tokenized_tweet = []
             for sentence in sentences:
                 words = nltk.word_tokenize(sentence)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                tokenized_tweet += words
-<<<<<<< HEAD
-
-            tokenized.append(str(tokenized_tweet))
-
-        return tokenized
-=======
-            
-            tokenized.append(tokenized_tweet)
-
-        return tokenized
->>>>>>> 2854caf (modified files and testing added)
-=======
-=======
->>>>>>> 7105dbc (resolve the conflict)
-=======
->>>>>>> 828d2d3 (fix merge issues)
                 words = self._delete_mentions(words)
                 remove_urls_hashtags_mentions = [
                     word for word in words if word[0] not in self.punctuation and word[:4] not in self.urls and word[:3] not in self.urls]
@@ -132,20 +57,3 @@ class Tokenizer(Preprocessor):
                     no_mentions.append(words[i])
 
         return no_mentions
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> a3333a8 (modified the tokenizer)
-=======
->>>>>>> 0898e45 (add the lemmatizer and its test)
-=======
-=======
-                tokenized_tweet += words
-            
-            tokenized.append(tokenized_tweet)
-
-        return tokenized
->>>>>>> 49c39fa (resolve the conflict)
->>>>>>> 7105dbc (resolve the conflict)
-=======
->>>>>>> 828d2d3 (fix merge issues)
