45da034aba0d45b495bb8b0e8c8f76fb6a9e8c0a
Reem Farah <reem.farah@visable.com>
2021-10-29 16:34:59 +0200

    modify bigram

--- code/feature_extraction/bigrams.py
+++ code/feature_extraction/bigrams.py
@@ -15,12 +15,48 @@ class BigramFeature(FeatureExtractor):
     
     def __init__(self, input_column):
         super().__init__([input_column], "{0}_bigrams".format(input_column))
+
     
     def _set_variables(self, inputs):
+        all_the_tweets = []
+        for tweet in inputs:
+            tokens = ast.literal_eval(tweet.item())
+    
+            all_the_tweets += tokens
         
-        overall_text = []
-        for line in inputs:
-            tokens = ast.literal_eval(line.item())
-            overall_text += tokens
+        self._bigrams_of_all_tweets = nltk.bigrams(all_the_tweets)
+        self._freq_dist = nltk.FreqDist(self._bigrams_of_all_tweets)
+        self._dictionary_of_all_tweets = {item[0]:item[1] for item in self._freq_dist.items()}
+
+        freq_list = []
+
+        for tweet in inputs:
+            tweet_bigram_freq = []
+            tweet = ast.literal_eval(tweet.item())
+            bigrams = list(nltk.bigrams(tweet))
+
+            for bigram in bigrams:
+                tweet_bigram_freq.append((bigram,self._dictionary_of_all_tweets.get(bigram)))
+
+            tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
+            print(tweet_bigram_freq)
+            
+            freq_list.append(tweet_bigram_freq)
+        return freq_list
+ 
+
+    # def _get_values(self, inputs):
+    #     freq_list = []
+
+    #     for tweet in inputs:
+    #         tweet_bigram_freq = []
+    #         bigrams = nltk.bigrams(tweet)
+    #         for bigram in bigrams:
+    #             tweet_bigram_freq.append((bigram,self._dictionary_of_all_tweets(bigram)))
+    #         tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
+            
+    #         freq_list.append(tweet_bigram_freq)
+    #     print("here")
+    #     return freq_list
+
         
-        self._bigrams = nltk.bigrams(overall_text)
