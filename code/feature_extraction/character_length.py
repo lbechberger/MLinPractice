a5a40d0cfa99c444eb4229d946aa745e08d16dc7
Reem Farah <reem.farah@visable.com>
2021-10-30 03:32:07 +0200

    character length modified

--- code/feature_extraction/character_length.py
+++ code/feature_extraction/character_length.py
@@ -12,17 +12,18 @@ import numpy as np
 from code.feature_extraction.feature_extractor import FeatureExtractor
 
 # class for extracting the character-based length as a feature
+
+
 class CharacterLength(FeatureExtractor):
-    
+
     # constructor
     def __init__(self, input_column):
         super().__init__([input_column], "{0}_charlength".format(input_column))
-    
+
     # don't need to fit, so don't overwrite _set_variables()
-    
+
     # compute the word length based on the inputs
     def _get_values(self, inputs):
-        
         result = np.array(inputs[0].str.len())
-        result = result.reshape(-1,1)
+        result = result.reshape(-1, 1)
         return result
