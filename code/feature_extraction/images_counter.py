76705b749d8e59dde27b9d4daa8a87807d252bac
Reem Farah <reem.farah@visable.com>
2021-10-31 12:38:30 +0100

    add images counter and its test

--- code/feature_extraction/images_counter.py
+++ code/feature_extraction/images_counter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Simple feature that counts the number of characters in the given column.
+Simple feature that counts the number of images in the given column.
 
 Created on Wed Sep 29 12:29:25 2021
 
@@ -11,18 +11,13 @@ import ast
 import numpy as np
 from code.feature_extraction.feature_extractor import FeatureExtractor
 
-# class for extracting the character-based length as a feature
 
-
-class Images(FeatureExtractor):
+class ImagesCounter(FeatureExtractor):
 
     # constructor
     def __init__(self, input_column):
-        super().__init__([input_column], "{0}_attached".format(input_column))
+        super().__init__([input_column], "{0}_shared".format(input_column))
 
-    # don't need to fit, so don't overwrite _set_variables()
-
-    # compute the word length based on the inputs
     def _get_values(self, inputs):
         result = np.array(inputs[0].apply(lambda x: len(ast.literal_eval(x))))
         result = result.reshape(-1, 1)
