68e110103cd5107c3efaa67da41a63dab8b6a040
Dennis Hesenkamp <83503798+dhesenkamp@users.noreply.github.com>
2021-10-31 19:04:58 +0100

    Updated classifier to work for param optimization

--- code/classification.sh
+++ code/classification.sh
@@ -4,13 +4,16 @@
 mkdir -p data/classification/
 
 # run feature extraction on training set (may need to fit extractors)
-echo "  training set"
+for i in "linear" "poly" "rbf" "sigmoid"
+do
+    echo "  training set"
 #python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --svm 'rbf' -s 42 -a -k -f1 -ba
-python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --svm "rbf" -s 42 -a -k -f1 -ba
+    python -m code.classification.run_classifier data/feature_extraction/training.pickle -e data/classification/classifier.pickle --svm $i -s 42 -a -k -f1 -ba
+
 
 # run feature extraction on validation set (with pre-fit extractors)
-echo "  validation set"
+    echo "  validation set"
 #python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle -a -k -f1 -ba
-python -m code.classification.run_classifier data/feature_extraction/validation.pickle -i data/classification/classifier.pickle -a -k -f1 -ba
-
+    python -m code.classification.run_classifier data/feature_extraction/validation.pickle -i data/classification/classifier.pickle -a -k -f1 -ba
+done
 # don't touch the test set, yet, because that would ruin the final generalization experiment!
--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -34,7 +34,8 @@ parser.add_argument("-f", "--frequency", action = "store_true", help = "label fr
 parser.add_argument("-u", "--uniform", action = "store_true", help = "uniform (random) classifier")
 parser.add_argument("--knn", type = int, help = "k nearest neighbor classifier with the specified value of k", default = None)
 parser.add_argument("--knn_weights", type = str, help = "weight function of knn, uniform or distance", default = "uniform")
-parser.add_argument("--tree", type = int, help = "decision tree classifier with the specified value as max depth", default = None)
+parser.add_argument("--tree", action = "store_true", help = "decision tree classifier", default = None)
+parser.add_argument("--tree_depth", type = int, help = "max depth of decision tree", default = None)
 parser.add_argument("--tree_criterion", type = str, help = "criterion to measure split quality, gini or entropy", default = "gini")
 parser.add_argument("--svm", type = str, help = "support vector machine with specified kernel: linear, polynomial, rbf, or sigmoid", default = None)
 parser.add_argument("--randforest", type = int, help = "random forest classifier with specified value as # of trees in forest", default = None)
@@ -83,19 +84,20 @@ else:   # manually set up a classifier
     elif args.frequency:
         # label frequency classifier
         print("    label frequency classifier")
-        log_param("classifier", "frequency")
-        params = {"classifier": "frequency"}
+        log_param("classifier", "stratified")
+        params = {"classifier": "stratified"}
         classifier = DummyClassifier(strategy = "stratified", random_state = args.seed)
         
     elif args.uniform:
         # uniform classifier
         print("    uniform classifier")
+        log_param("classifier", "uniform")
+        params = {"classifier": "uniform"}
         classifier = DummyClassifier(strategy = "uniform", random_state = args.seed)
-        classifier.fit(data["features"], data["labels"])
     
     elif args.knn is not None:
         # k nearest neighbour classifier
-        print("    {0} nearest neighbor classifier with {1} weights".format(args.knn, args.knn_weights))
+        print("    {0} nearest neighbor classifier, {1} weights".format(args.knn, args.knn_weights))
         
         log_param("classifier", "knn")
         log_param("k", args.knn)
@@ -110,17 +112,17 @@ else:   # manually set up a classifier
         
     elif args.tree is not None:
         # decision tree classifier
-        print("    decision tree with max depth of {0} and {1} split criterion".format(args.tree, args.tree_criterion))
+        print("    decision tree with max depth {0}, {1} split criterion".format(args.tree_depth, args.tree_criterion))
         
         log_param("classifier", "tree")
         log_param("criterion", args.tree_criterion)
-        log_param("max_depth", args.tree)
+        log_param("max_depth", args.tree_depth)
         params = {"classifier": "tree", 
                   "criterion": args.tree_criterion, 
-                  "max_depth": args.tree}
+                  "max_depth": args.tree_depth}
         
         #standardizer = StandardScaler()
-        classifier = DecisionTreeClassifier(criterion = args.tree_criterion, max_depth = args.tree)
+        classifier = DecisionTreeClassifier(criterion = args.tree_criterion, max_depth = args.tree_depth)
         #classifier = make_pipeline(standardizer, decision_tree)
     
     elif args.svm is not None:
@@ -184,13 +186,13 @@ prediction = classifier.predict(data["features"])
 # collect all evaluation metrics
 evaluation_metrics = []
 if args.accuracy:
-    evaluation_metrics.append(("accuracy", accuracy_score))
+    evaluation_metrics.append(("Accuracy", accuracy_score))
 if args.kappa:
-    evaluation_metrics.append(("Cohen_kappa", cohen_kappa_score))
+    evaluation_metrics.append(("Cohens_kappa", cohen_kappa_score))
 if args.f1_score:
     evaluation_metrics.append(("F1_score", f1_score))
 if args.balanced_accuracy:
-    evaluation_metrics.append(("balanced_accuracy", balanced_accuracy_score))
+    evaluation_metrics.append(("Balanced_accuracy", balanced_accuracy_score))
 
 # compute and print them
 for metric_name, metric in evaluation_metrics:
