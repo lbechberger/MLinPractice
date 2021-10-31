da9e147fd18826d78ed7c0f09fffda3dd08aca5e
Reem Farah <reem.farah@visable.com>
2021-10-31 21:25:24 +0100

    cleaned the code of bigrams

--- code/feature_extraction/bigrams.py
+++ code/feature_extraction/bigrams.py
@@ -16,31 +16,31 @@ class BigramFeature(FeatureExtractor):
     def __init__(self, input_column):
         super().__init__([input_column], "{0}_bigrams".format(input_column))
 
-    def _set_variables(self, inputs):
-        """Computes the frequency distribution of all bigrams in the tweets"""
-        all_the_tweets = []
-        for tweet in inputs[0]:
-            tokens = ast.literal_eval(tweet)
-            all_the_tweets += tokens
-        self._bigrams_of_all_tweets = list(nltk.bigrams(all_the_tweets))
-        self._freq_dist_of_all_tweets = nltk.FreqDist(
-            self._bigrams_of_all_tweets)
-        self._dictionary_of_all_tweets = {
-            item[0]: item[1] for item in self._freq_dist_of_all_tweets.items()}
+    # def _set_variables(self, inputs):
+    #     """Computes the frequency distribution of all bigrams in the tweets"""
+    #     all_the_tweets = []
+    #     for tweet in inputs[0]:
+    #         tokens = ast.literal_eval(tweet)
+    #         all_the_tweets += tokens
+    #     self._bigrams_of_all_tweets = list(nltk.bigrams(all_the_tweets))
+    #     self._freq_dist_of_all_tweets = nltk.FreqDist(
+    #         self._bigrams_of_all_tweets)
+    #     self._dictionary_of_all_tweets = {
+    #         item[0]: item[1] for item in self._freq_dist_of_all_tweets.items()}
 
-        self._freq_list = []
-        for tweet in inputs[0]:
-            tweet_bigram_freq = []
-            tweet = ast.literal_eval(tweet)
-            bigrams = list(nltk.bigrams(tweet))
-            for bigram in bigrams:
-                tweet_bigram_freq.append(
-                    (bigram, self._dictionary_of_all_tweets.get(bigram)))
-            # remove duplicates
-            tweet_bigram_freq = list(
-                filter(None, list(set(tweet_bigram_freq))))
-            tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
-            self._freq_list.append(tweet_bigram_freq)
+    #     self._freq_list = []
+    #     for tweet in inputs[0]:
+    #         tweet_bigram_freq = []
+    #         tweet = ast.literal_eval(tweet)
+    #         bigrams = list(nltk.bigrams(tweet))
+    #         for bigram in bigrams:
+    #             tweet_bigram_freq.append(
+    #                 (bigram, self._dictionary_of_all_tweets.get(bigram)))
+    #         # remove duplicates
+    #         tweet_bigram_freq = list(
+    #             filter(None, list(set(tweet_bigram_freq))))
+    #         tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
+    #         self._freq_list.append(tweet_bigram_freq)
 
     def _get_values(self, inputs):
         all_the_tweets = []
@@ -54,6 +54,9 @@ class BigramFeature(FeatureExtractor):
         self._dictionary_of_all_tweets = {
             item[0]: item[1] for item in self._freq_dist_of_all_tweets.items()}
 
+        # this list is only for testing purposes
+        self._freq_list_testing = []
+
         self._freq_list = []
         for tweet in inputs[0]:
             tweet_bigram_freq = []
@@ -67,6 +70,10 @@ class BigramFeature(FeatureExtractor):
             tweet_bigram_freq = list(
                 filter(None, list(set(tweet_bigram_freq))))
             tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
+            
+            # for testing
+            self._freq_list_testing.append(tweet_bigram_freq)
+
             frequencies = [frequency[1] for frequency in tweet_bigram_freq]
             # sum the frequencies
             self._freq_list.append(sum(frequencies))
--- test/feature_extraction/bigrams_test.py
+++ test/feature_extraction/bigrams_test.py
@@ -12,13 +12,25 @@ from code.feature_extraction.bigrams import BigramFeature
 
 
 class BigramFeatureTest(unittest.TestCase):
-
+    
     def setUp(self):
         self.INPUT_COLUMN = "input"
         self.bigram_feature = BigramFeature(self.INPUT_COLUMN)
         self.df = pd.DataFrame()
         self.df[self.INPUT_COLUMN] = ['["This", "is", "a", "tweet", "This", "is", "also", "a", "test"]',
-                                      '["This", "is", "a", "lovely" ,"cat", "very", "lovely", "cat"]']
+        '["This", "is", "a", "lovely" ,"cat", "very", "lovely", "cat"]']
+
+    
+    def test_input_columns(self):
+        self.assertEqual(self.bigram_feature._input_columns, [self.INPUT_COLUMN])
+
+    def test_feature_name(self):
+        self.assertEqual(self.bigram_feature.get_feature_name(), self.INPUT_COLUMN + "_bigrams")
+
+    def test_list_of_bigrams_exists(self):
+        self.bigram_feature.fit(self.df)
+        self.assertGreaterEqual(len(list(self.bigram_feature._bigrams_of_all_tweets)), 0)
+
 
     def test_input_columns(self):
         self.assertEqual(self.bigram_feature._input_columns,
@@ -29,29 +41,14 @@ class BigramFeatureTest(unittest.TestCase):
                          self.INPUT_COLUMN + "_bigrams")
 
     def test_list_of_bigrams_exists(self):
-        self.bigram_feature.fit(self.df)
-        self.assertGreaterEqual(
-            len(list(self.bigram_feature._bigrams_of_all_tweets)), 0)
+        self.bigram_feature.transform(self.df)
+        self.assertGreaterEqual(len(list(self.bigram_feature._bigrams_of_all_tweets)), 0)
 
-    def test_input_columns(self):
-        self.assertEqual(self.bigram_feature._input_columns,
-                         [self.INPUT_COLUMN])
-
-    def test_feature_name(self):
-        self.assertEqual(self.bigram_feature.get_feature_name(),
-                         self.INPUT_COLUMN + "_bigrams")
-
-    def test_list_of_bigrams_exists(self):
-        self.bigram_feature.fit(self.df)
-        self.assertGreaterEqual(
-            len(list(self.bigram_feature._bigrams_of_all_tweets)), 0)
 
     def test_list_of_bigrams_most_frequent_correct(self):
-        self.bigram_feature.fit(self.df)
-        EXPECTED_BIGRAM = [('This', 'is'), 3, ('is', 'a'),
-                           2, ('lovely', 'cat'), 2]
-        freq_dist = self.bigram_feature._freq_list
-
+        self.bigram_feature.transform(self.df)
+        EXPECTED_BIGRAM = [('This', 'is'), 3, ('is', 'a'), 2, ('lovely', 'cat'), 2]
+        freq_dist = self.bigram_feature._freq_list_testing
         for i in range(2):
             j = 0
             self.assertEqual(freq_dist[0][j][i], EXPECTED_BIGRAM[i])
@@ -60,3 +57,4 @@ class BigramFeatureTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    
