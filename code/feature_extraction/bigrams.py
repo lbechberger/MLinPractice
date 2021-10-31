c220d2028b44a570810e85e3ef2c61df9a1b5a23
Reem Farah <reem.farah@visable.com>
2021-10-31 22:35:21 +0100

    solve the error of the bigram

--- code/feature_extraction/bigrams.py
+++ code/feature_extraction/bigrams.py
@@ -16,38 +16,15 @@ class BigramFeature(FeatureExtractor):
     def __init__(self, input_column):
         super().__init__([input_column], "{0}_bigrams".format(input_column))
 
-    # def _set_variables(self, inputs):
-    #     """Computes the frequency distribution of all bigrams in the tweets"""
-    #     all_the_tweets = []
-    #     for tweet in inputs[0]:
-    #         tokens = ast.literal_eval(tweet)
-    #         all_the_tweets += tokens
-    #     self._bigrams_of_all_tweets = list(nltk.bigrams(all_the_tweets))
-    #     self._freq_dist_of_all_tweets = nltk.FreqDist(
-    #         self._bigrams_of_all_tweets)
-    #     self._dictionary_of_all_tweets = {
-    #         item[0]: item[1] for item in self._freq_dist_of_all_tweets.items()}
-
-    #     self._freq_list = []
-    #     for tweet in inputs[0]:
-    #         tweet_bigram_freq = []
-    #         tweet = ast.literal_eval(tweet)
-    #         bigrams = list(nltk.bigrams(tweet))
-    #         for bigram in bigrams:
-    #             tweet_bigram_freq.append(
-    #                 (bigram, self._dictionary_of_all_tweets.get(bigram)))
-    #         # remove duplicates
-    #         tweet_bigram_freq = list(
-    #             filter(None, list(set(tweet_bigram_freq))))
-    #         tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
-    #         self._freq_list.append(tweet_bigram_freq)
-
     def _get_values(self, inputs):
         all_the_tweets = []
         # need to recompute the frequency distribution, case we have new input as the validation set
         for tweet in inputs[0]:
-            tokens = ast.literal_eval(tweet)
-            all_the_tweets += tokens
+            if not isinstance(tweet, list):
+                tokens = ast.literal_eval(tweet)
+                all_the_tweets += tokens
+            else:
+                all_the_tweets = tokens
         self._bigrams_of_all_tweets = list(nltk.bigrams(all_the_tweets))
         self._freq_dist_of_all_tweets = nltk.FreqDist(
             self._bigrams_of_all_tweets)
@@ -70,7 +47,7 @@ class BigramFeature(FeatureExtractor):
             tweet_bigram_freq = list(
                 filter(None, list(set(tweet_bigram_freq))))
             tweet_bigram_freq.sort(key=lambda x: x[1], reverse=True)
-            
+
             # for testing
             self._freq_list_testing.append(tweet_bigram_freq)
 
