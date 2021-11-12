787b4275e4de4a7eaa2e13acce0f9d9b570ed0ef
Christopher Bröcker <broecker.christopher@gmail.com>
2021-11-12 16:34:15 +0100

    fix feature_extraction/keyword_test

--- test/feature_extraction/keyword_test.py
+++ test/feature_extraction/keyword_test.py
@@ -17,29 +17,28 @@ class KeywordTest(unittest.TestCase):
     def setUp(self):
         self.INPUT_COLUMN = "input_column"
         self.LABEL_COLUMN = "label_column"
-        self.number_of_words = 3
-        self.inputs = [["test", "test"], ["doof","test"], ["ml","doof"]]
+        self.number_of_keywords = 3
+        self.inputs = [["test", "test"], ["doof","test"], ["ml","doof"], ["Not viral","ml"]]
+        self.labels = [True,True,True,False]
         self.expected_variables = [("test", 3), ("doof", 2), ("ml",1)]
         self.expected_outputs = [[1,0,0], [1,1,0], [0,1,1]]
-        self.keyword =Keyword([self.INPUT_COLUMN, self.LABEL_COLUMN], self.number_of_keywords)
-        self.keyword._set_variables(self.inputs)
+        self.keyword = Keyword([self.INPUT_COLUMN, self.LABEL_COLUMN], self.number_of_keywords)
+        self.keyword._set_variables([self.inputs, self.labels])
 
     def test_keyword_set_variables(self):
         test_value = [self.expected_variables == self.keyword.keywords]
         self.assertTrue(test_value)
 
     def test_keywords_get_values(self):
-        function_output = self.keywords._get_values(self.inputs)
+        function_output = self.keyword._get_values(self.inputs)
         test_value = [self.expected_outputs == function_output]
         self.assertTrue(test_value)
-    
-    def test_keywords_set_variables(self)
 
     def test_input_columns(self):
-        self.assertEqual(self.keywords._input_columns, [self.INPUT_COLUMN, self.LABEL_COLUMN])
+        self.assertEqual(self.keyword._input_columns, [self.INPUT_COLUMN, self.LABEL_COLUMN])
 
     def test_feature_name(self):
-        self.assertEqual(self.keywords.get_feature_name(), "keywords")
+        self.assertEqual(self.keyword.get_feature_name(), "keywords")
 
 
 if __name__ == '__main__':
