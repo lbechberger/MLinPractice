b5511bb83263b5b19c4dc8c954ec60883d7498ab
Christopher Bröcker <broecker.christopher@gmail.com>
2021-11-12 17:46:36 +0100

    remove dimensionality reduction by using -m  35

--- src/dimensionality_reduction.sh
+++ src/dimensionality_reduction.sh
@@ -5,7 +5,7 @@ mkdir -p data/dimensionality_reduction/
 
 # run dimensionality reduction on training set to fit the parameters
 echo "  training set"
-python -m src.dimensionality_reduction.reduce_dimensionality data/feature_extraction/training.pickle data/dimensionality_reduction/training.pickle -e data/dimensionality_reduction/pipeline.pickle -m 15 --verbose
+python -m src.dimensionality_reduction.reduce_dimensionality data/feature_extraction/training.pickle data/dimensionality_reduction/training.pickle -e data/dimensionality_reduction/pipeline.pickle -m 35 --verbose
 
 # run feature extraction on validation set and test set (with pre-fit parameters)
 echo "  validation set"
